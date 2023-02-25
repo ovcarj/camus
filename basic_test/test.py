8b213d27dcdbe97d5b4f61b9cd119b94cffec6a2
ovcarj <juraj.ovcar@gmail.com>
2023-02-25 12:47:01 +0100

    Added basic test

---
+++ basic_test/test.py
@@ -0,0 +1,22 @@
+from ase.io import read
+from camus import camus
+
+structs = read('test.traj', index='0:100')
+
+camus_object = camus.Camus(structs)
+
+print(f'Read {len(camus_object.Cstructures.structures)} structures into a camus.Cstructures object.')
+
+camus_object.Cstructures.create_datasets()
+
+print(f'Created ML datasets. {len(camus_object.Cstructures.test_set)} structures are in the test set.')
+
+empty = camus.Camus(structs)
+
+print(f'Created camus object without any structures.')
+
+empty.Cstructures.structures = structs
+
+print(f'Added {len(empty.Cstructures.structures)} to the empty camus object.')
+
+print('Test OK')
