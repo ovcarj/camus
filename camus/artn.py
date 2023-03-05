6c7b581b006eba0e1ff96eb834554bea99926e64
ovcarj <juraj.ovcar@gmail.com>
2023-03-05 19:21:24 +0100

    Wrote write_sisyphus_script method

--- camus/artn.py
+++ camus/artn.py
@@ -184,7 +184,7 @@ class ARTn:
             'minimize': '1e-4 1e-5 5000 10000',
              }
 
-        # Default parameters for the initial Sisyphus calculation
+        # Default parameters for the initial Sisyphus potential energy calculation
         default_initial_sisyphus = {
             'units': 'metal',
             'dimension': '3',
@@ -211,8 +211,7 @@ class ARTn:
             input_parameters = default_initial_sisyphus
 
         # Set self._lammps_parameters
-        for key, value in input_parameters.items():
-            self._lammps_parameters[key] = value
+        self._lammps_parameters = input_parameters
 
 
     def write_lammps_in(self, target_directory=None, filename='lammps.in'):
@@ -254,6 +253,41 @@ class ARTn:
         with open(os.path.join(target_directory, filename), 'w') as f:
             f.write(lammps_in_content)
 
+    def set_sisyphus_parameters(self, **kwargs):
+        """ Method that sets parameters to be written in sisyphus.sh to self._sisyphus_parameters dictionary.
+
+        Parameters:
+           dE_initial_threshold: Threshold for reaching the top of the hill [eV]
+           dE_final_threshold: The search stops when E_current_minimum - E_initial <= dE_final_threshold (activated only after the top of the hill is passed) [eV]
+           delr_threshold: At least one of the delr's has to be smaller than this, otherwise the search path is not connected and we restart ARTn [angstrom]
+           maximum_steps: Maximum number of ARTn searches before we give up
+           run_lammps: Command to run the LAMMPS executable (e.g. mpirun -np 1). If not given, defaults to the $RUN_LAMMPS environment variable. (Still requires testing)
+           lammps_exe: Path to the LAMMPS executable. If not given, defaults to the $LAMMPS_EXE environment variable
+           sisyphus_functions_path: Path to the sisyphus bash functions. If not given, defaults to $CAMUS_BASE/sisyphus_files/sisyphus_functions
+           initial_lammps_in: Filename of the input file for initial LAMMPS minimization/PE calculation
+           initial_lammps_out: Filename of the output file for initial LAMMPS minimization/PE calculation
+
+        """
+        default_parameters= {
+            'dE_initial_threshold': '0.5',
+            'dE_final_threshold': '0.1',
+            'delr_threshold': '1.0',
+            'maximum_steps': '100',
+            'run_lammps': os.environ.get('RUN_LAMMPS'),
+            'lammps_exe': os.environ.get('LAMMPS_EXE'),
+            'sisyphus_functions_path': os.path.join(os.environ.get('CAMUS_BASE'), 'sisyphus_files/sisyphus_functions'),
+            'initial_lammps_in': 'initial_lammps.in',
+            'initial_lammps_out': 'initial_lammps.out' 
+            }
+
+        for key in kwargs:
+            if key not in default_parameters:
+                raise RuntimeError('Unknown keyword: %s' % key)
+
+        # Set self._sisyphus_parameters
+        for key, value in default_parameters.items():
+            self._sisyphus_parameters[key] = kwargs.pop(key, value)
+
     def write_sisyphus_script(self, target_directory=None, filename='sisyphus.sh'):
         """ Method that writes the main Sisyphus bash script to `target directory/filename` using self._sisyphus_parameters.
         If `target_directory` is not given, `CAMUS_ARTN_DATA_DIR` environment variable will be used.
@@ -262,7 +296,6 @@ class ARTn:
         Parameters:
             target_directory: directory in which to write the lammps.in file
             filename: name of the Sisyphus bash script
-
 
         """
         # Set the default target directory to CAMUS_ARTN_DATA_DIR environment variable
@@ -279,4 +312,90 @@ class ARTn:
         if not self.sisyphus_parameters:
             self.set_sisyphus_parameters()
 
+        # Write the sisyphus.sh file to the target directory
+        with open(os.path.join(target_directory, filename), 'w') as f:
+            f.write(f"""#!/bin/bash
 
+source {self.sisyphus_parameters['sisyphus_functions_path']}
+
+{self.sisyphus_parameters['run_lammps']} {self.sisyphus_parameters['lammps_exe']} -in {self.sisyphus_parameters['initial_lammps_in']} > {self.sisyphus_parameters['initial_lammps_out']} 
+
+E_initial=$(grep "PotEng" initial_lammps.out -A1 | tail -1 | awk {{'print $2'}})
+
+dE_initial=-1.0           # Difference between current minimum and initial energy [eV]
+dE_initial_threshold={self.sisyphus_parameters['dE_initial_threshold']}  # Threshold for reaching the top of the hill [eV]
+dE_final_threshold={self.sisyphus_parameters['dE_final_threshold']}    # The search stops when E_current minimum - E_initial <= dE_final_threshold (activated only after the top of the hill is passed) [eV]
+
+delr_threshold={self.sisyphus_parameters['delr_threshold']}	  # At least one of the delr's has to be smaller than this, otherwise the search path is not connected and we restart ARTn [angstrom]
+
+outer_counter=0           # Counting how many times an ARTn search is called (first call is labelled 0)
+minima_counter=0          # Counting how many minimas (and saddle points) are accepted
+basin_counter=0           # Counting how many times we started an ARTn search from the same configuration
+maximum_steps={self.sisyphus_parameters['maximum_steps']}         # Maximum number of ARTn searches before we give up.
+
+E_current=$E_initial      # Current configuration energy is initialized to the initial energy
+
+define_file_names         # Define prefix "${{dE_initial_threshold}}_${{dE_final_threshold}}" for this run
+cleanup_dat               # This will delete the *.dat files containing information of a previous run
+cleanup_minima            # This will delete minimum_*.xyz, saddlepoint_*.xyz and LAMMPS data files from previous runs
+initialize_files          # Create *.dat files for a fresh run
+
+
+
+echo "$E_initial" >> $MINIMA_ENERGIES_FILE
+echo "$E_initial" >> $ALL_ENERGIES_FILE
+
+
+ACCEPTANCE_SIGN=">"       # First we want to climb up the hill
+REJECTION_SIGN="<"        # First we want to climb up the hill
+
+
+while (( $(echo "$dE_initial < $dE_initial_threshold" | bc -l) ))
+do
+    if (( $(echo "$outer_counter < $maximum_steps" | bc -l) ))
+    then
+        echo "MSG_$outer_counter: Running ARTn search #$outer_counter ..." >> $HILL_LOG_FILE
+        mpirun -np 1 ./lmp_mpi -in lammps.in
+
+        advance_search
+    else
+        echo "MSG_FAIL: Maximum number of searches ($maximum_steps) exceeded while climbing up the hill. Exiting." >> $HILL_LOG_FILE
+        exit
+    fi
+done
+
+echo "MSG_TOP: Top of the hill passed." >> $HILL_LOG_FILE
+echo "MSG_TOP: Top of the hill minimum index: $minima_counter" >> $HILL_LOG_FILE
+
+E_top=$E_current
+Activation_E_forward=$(echo "$E_top - $E_initial" | bc -l)
+echo "MSG_TOP: E_initial = $E_initial    E_top = $E_top    Activation_E_forward = $Activation_E_forward" >> $HILL_LOG_FILE
+
+
+ACCEPTANCE_SIGN="<"       # Now we're going down the hill
+REJECTION_SIGN=">"        # Now we're going down the hill
+
+
+while (( $(echo "$dE_initial >= $dE_final_threshold" | bc -l) ))
+do
+    if (( $(echo "$outer_counter < $maximum_steps" | bc -l) ))
+    then
+        echo "MSG_$outer_counter: Running ARTn search #$outer_counter ..." >> $HILL_LOG_FILE
+        mpirun -np 1 ./lmp_mpi -in lammps.in
+
+        advance_search
+    else
+        echo "MSG_FAIL: Maximum number of searches ($maximum_steps) exceeded while going down the hill. Exiting." >> $HILL_LOG_FILE
+        exit
+    fi
+done
+
+echo "MSG_END: Threshold satisfied. Ending search." >> $HILL_LOG_FILE
+echo "MSG_END: Final minimum index: $minima_counter" >> $HILL_LOG_FILE
+
+E_final=$E_current
+Delta_E_final_top=$(echo "$E_final - $E_top" | bc -l)
+Delta_E_final_initial=$(echo "$E_final - $E_initial" | bc -l)
+
+echo "MSG_END: E_initial = $E_initial    E_top = $E_top    E_final = $E_final" >> $HILL_LOG_FILE
+echo "MSG_END: Activation_E_forward = $Activation_E_forward    Delta_E_final_top = $Delta_E_final_top    Delta_E_final_initial = $Delta_E_final_initial" >> $HILL_LOG_FILE""")
