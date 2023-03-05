df589a75b67bc7481041f2cca52d532c63b7a071
ovcarj <juraj.ovcar@gmail.com>
2023-03-05 15:14:06 +0100

    Added environment variables related to LAMMPS execution

--- env_vars.sh
+++ env_vars.sh
@@ -1,6 +1,12 @@
 #/bin/bash
 
 # Bash script to automatically define all necessary variables to run CAMUS in .bashrc
+# LAMMPS_EXE variable must be set
+# RUN_LAMMPS variable (e.g. mpirun -np 8) should be made optional (not yet tested)
+
+echo '' >> ~/.bashrc
+echo '# >>> Initialize CAMUS environment variables >>>' >> ~/.bashrc
+echo '' >> ~/.bashrc
 
 # Define python path
 WD=$(pwd)
@@ -14,3 +20,12 @@ echo 'export CAMUS_LAMMPS_DATA_DIR=~/.CAMUS/LAMMPS_data' >> ~/.bashrc
 
 # Define default ARTn data target directory
 echo 'export CAMUS_ARTN_DATA_DIR=~/.CAMUS/ARTn_data' >> ~/.bashrc
+
+# Define LAMMPS execution command
+echo 'export RUN_LAMMPS=""' >> ~/.bashrc
+
+# Define path to LAMMPS executable 
+echo 'export LAMMPS_EXE=/path/to/lammps/executable' >> ~/.bashrc
+
+echo '' >> ~/.bashrc
+echo '# <<< Initialize CAMUS environment variables <<<' >> ~/.bashrc
